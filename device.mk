d19e701138e30911b8fb3509b01485bc68047738
LibXZR <i@xzr.moe>
2023-01-09 21:00:52 +0000

    kebab: Build naruto light ALS correction service
    Signed-off-by: AnierinB <anierin@evolution-x.org>

--- device.mk
+++ device.mk
@@ -34,6 +34,10 @@ PRODUCT_PACKAGES += \
     OPlusSettingsProviderResTarget \
     OPlusSystemUIResTarget
 
+# Sensors
+PRODUCT_PACKAGES += \
+    als_correction_service.oplus
+
 # Shipping API
 PRODUCT_SHIPPING_API_LEVEL := 30
 
